5447e132ca2831c25ec33e116fffd69a4ac2eee9
Tommaso.Boschi <tboschi@heppc402.ph.qmul.ac.uk>
2020-06-03 12:07:18 +0100

    added pure.sh

---
+++ pure.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+#./prepare_systematics.sh -r /data/tboschi/HKsens/errorstudy/test0   DataFit_Postfit_2018_final_v1_180907_sk_eb_valor_order_all_plus_scc.root SKJointErrorMatrix2018_Total_fitqun_v4_16thAug2017_VALOR_order.root
+#./prepare_systematics.sh -r /data/tboschi/HKsens/errorstudy/0   DataFit_Postfit_2018_final_v1_180907_sk_eb_valor_order_all_plus_scc.root SKJointErrorMatrix2018_Total_fitqun_v4_16thAug2017_VALOR_order.root
+#./prepare_systematics.sh -r /data/tboschi/HKsens/errorstudy/8   banff_increasednueflux.root SKJointErrorMatrix2018_Total_fitqun_v4_16thAug2017_VALOR_order.root
+#./prepare_systematics.sh -r /data/tboschi/HKsens/errorstudy/11a DataFit_Postfit_2018_final_v1_180907_sk_eb_valor_order_all_plus_scc.root skdetfsi_escale29.root
+#./prepare_systematics.sh -r /data/tboschi/HKsens/errorstudy/11b DataFit_Postfit_2018_final_v1_180907_sk_eb_valor_order_all_plus_scc.root skdetfsi_escale19.root
+#./prepare_systematics.sh -r /data/tboschi/HKsens/errorstudy/flux_lukas  banff_extraflux.root SKJointErrorMatrix2018_Total_fitqun_v4_16thAug2017_VALOR_order.root
+
+#./prepare_systematics.sh -r /data/tboschi/HKsens/errorstudy/nuenorm1_corr norm_nue1_nuebar1_corr.root
+#./prepare_systematics.sh -r /data/tboschi/HKsens/errorstudy/nuenorm2_corr norm_nue2_nuebar2_corr.root
+#./prepare_systematics.sh -r /data/tboschi/HKsens/errorstudy/nuenorm3_corr norm_nue3_nuebar3_corr.root
+#./prepare_systematics.sh -r /data/tboschi/HKsens/errorstudy/nuenorm4_corr norm_nue4_nuebar4_corr.root
+#./prepare_systematics.sh -r /data/tboschi/HKsens/errorstudy/nuenorm5_corr norm_nue5_nuebar5_corr.root
+#./prepare_systematics.sh -r /data/tboschi/HKsens/errorstudy/nuenorm1_anti norm_nue1_nuebar1_anticorr.root
+#./prepare_systematics.sh -r /data/tboschi/HKsens/errorstudy/nuenorm2_anti norm_nue2_nuebar2_anticorr.root
+#./prepare_systematics.sh -r /data/tboschi/HKsens/errorstudy/nuenorm3_anti norm_nue3_nuebar3_anticorr.root
+#./prepare_systematics.sh -r /data/tboschi/HKsens/errorstudy/nuenorm4_anti norm_nue4_nuebar4_anticorr.root
+#./prepare_systematics.sh -r /data/tboschi/HKsens/errorstudy/nuenorm5_anti norm_nue5_nuebar5_anticorr.root
+#./prepare_systematics.sh -r /data/tboschi/HKsens/errorstudy/nuenorm1_indy norm_nue1_nuebar1_corr.root
+#./prepare_systematics.sh -r /data/tboschi/HKsens/errorstudy/nuenorm2_indy norm_nue2_nuebar2_corr.root
+#./prepare_systematics.sh -r /data/tboschi/HKsens/errorstudy/nuenorm3_indy norm_nue3_nuebar3_corr.root
+#./prepare_systematics.sh -r /data/tboschi/HKsens/errorstudy/nuenorm4_indy norm_nue4_nuebar4_corr.root
+#./prepare_systematics.sh -r /data/tboschi/HKsens/errorstudy/nuenorm5_indy norm_nue5_nuebar5_corr.root
+
+
+./prepare_systematics.sh -r /data/tboschi/HKsens/errorstudy/6a  banff_nonu2p2h_remove.root SKJointErrorMatrix2018_Total_fitqun_v4_16thAug2017_VALOR_order.root
+./prepare_systematics.sh -r /data/tboschi/HKsens/errorstudy/7a  banff_nonubar2p2h_remove.root SKJointErrorMatrix2018_Total_fitqun_v4_16thAug2017_VALOR_order.root
+./prepare_systematics.sh -r /data/tboschi/HKsens/errorstudy/67  banff_nonu2p2h_nonubar2p2h_remove.root SKJointErrorMatrix2018_Total_fitqun_v4_16thAug2017_VALOR_order.root
