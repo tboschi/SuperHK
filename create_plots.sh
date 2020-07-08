a31ea956fb3c105fdedfacd84bdc3304e03b0f2b
tboschi <tommaso.boschi@qmul.ac.uk>
2020-07-08 12:13:47 +0100

    script produces pdflatex file too

--- create_plots.sh
+++ create_plots.sh
@@ -4,20 +4,28 @@ dest=${1%/}
 
 option="dataset=\"$dest\""
 
-gnuplot -e $option plot/plot_dCP.glp
-gnuplot -e $option plot/plot_M23.glp
-gnuplot -e $option plot/plot_S23.glp
-gnuplot -e $option plot/plot_S13.glp
+echo option $option
 
-gnuplot -e $option plot/cont_dCP_M23.glp
-gnuplot -e $option plot/cont_S13_dCP.glp
-gnuplot -e $option plot/cont_S23_dCP.glp
-gnuplot -e $option plot/cont_S13_M23.glp
-gnuplot -e $option plot/cont_S23_M23.glp
-gnuplot -e $option plot/cont_S13_S23.glp
+cd plot
 
-gnuplot -e $option plot/sens.glp
-gnuplot -e $option plot/sens_detail.glp
+gnuplot -e "$option" plot_dCP.gpl
+gnuplot -e "$option" plot_M23.gpl
+gnuplot -e "$option" plot_S23.gpl
+gnuplot -e "$option" plot_S13.gpl
 
-echo mv -f $dest"*.*" plot/$dest/
-mv -f $dest*.* plot/$dest/
+gnuplot -e "$option" cont_dCP_M23.gpl
+gnuplot -e "$option" cont_S13_dCP.gpl
+gnuplot -e "$option" cont_S23_dCP.gpl
+gnuplot -e "$option" cont_S13_M23.gpl
+gnuplot -e "$option" cont_S23_M23.gpl
+gnuplot -e "$option" cont_S13_S23.gpl
+
+gnuplot -e "$option" sens.gpl
+gnuplot -e "$option" sens_detail.gpl
+
+echo mv -f $dest"*.*" $dest/
+mv -f $dest*.* $dest/
+
+cd ..
+
+./bin/plot_bundle plot/$dest/$dest'_'*.tex
