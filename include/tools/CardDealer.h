f8b1a7bea2fd7d71c93b7cc57f9b7974fe929edc
tboschi <tommaso.boschi@qmul.ac.uk>
2020-08-13 13:56:18 +0100

    templated card dealer compatible with vector, list, set, deque

--- include/tools/CardDealer.h
+++ include/tools/CardDealer.h
@@ -1,5 +1,6 @@
 /* CardDealer
  * handles cards
+ * Templated version
  */
 
 #ifndef CardDealer_H
@@ -8,71 +9,233 @@
 #include <cstdlib>
 #include <map>
 #include <vector>
+#include <set>
+#include <list>
+#include <deque>
 #include <fstream>
 #include <iostream>
 #include <sstream>
 #include <string>
 #include <algorithm>
+#include <type_traits>
 
-// trim from start (in place)
-static inline void ltrim(std::string &s)
-{
-	s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int ch) {
-				return !std::isspace(ch); }));
-}
-
-// trim from end (in place)
-static inline void rtrim(std::string &s)
-{
-	s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) {
-				return !std::isspace(ch); }).base(), s.end());
-}
-
-// trim from both ends (in place)
-static inline void trim(std::string &s)
-{
-	ltrim(s);
-	rtrim(s);
-}
+//// trim from start (in place)
+//static inline void ltrim(std::string &s)
+//{
+//	s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int ch) {
+//				return !std::isspace(ch); }));
+//}
+//
+//// trim from end (in place)
+//static inline void rtrim(std::string &s)
+//{
+//	s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) {
+//				return !std::isspace(ch); }).base(), s.end());
+//}
+//
+//// trim from both ends (in place)
+//static inline void trim(std::string &s)
+//{
+//	ltrim(s);
+//	rtrim(s);
+//}
 
 class CardDealer
 {
 	public:
-		CardDealer(std::string cardFile, bool verb = false);
-		CardDealer(char* filename, bool verb = true);
+		template <typename T>
+		CardDealer(T cardFile, bool verb = false) :
+			_cardFile(cardFile),
+			kVerbosity(verb) {
+			Parse(_cardFile);
+		}
 
-		std::string CardName();
+		std::string CardName() {
+			return _cardFile;
+		}
 
-		bool Status();
-		bool ReadCard(const std::string cardFile);
+		bool Parse(const std::string &cardFile) {
+			_entries.clear();
 
-		bool Find(const std::string key);
+			std::ifstream inf(cardFile.c_str());
 
-		bool Get(const std::string key, int &ii);
-		bool Get(const std::string key, double &dd);
-		bool Get(const std::string key, std::string & ss);
-		bool Get(const std::string key, std::vector<int>& vi);
-		bool Get(const std::string key, std::vector<double>& vd);
-		bool Get(const std::string key, std::vector<std::string>& vs);
-		bool Get(const std::string key, std::map<std::string, int>& mi);
-		bool Get(const std::string key, std::map<std::string, double>& mv);
-		bool Get(const std::string key, std::map<std::string, std::string>& ms);
-		bool Get(const std::string key, std::map<std::string, std::vector<int> >& mi);
-		bool Get(const std::string key, std::map<std::string, std::vector<double> >& mv);
-		bool Get(const std::string key, std::map<std::string, std::vector<std::string> >& ms);
+			if(!inf.is_open()) {
+				std::string msg = "ERROR CardDealer: " + cardFile + "does not exist";
+				throw std::invalid_argument(msg);
+			}
+			else if (kVerbosity)
+				std::cout << "CardDealer: reading input from " << cardFile << std::endl;
 
-		std::vector<std::string> ListKeys();
-		std::vector<std::string> ListDoubleKeys();
-		std::vector<std::string> ListStringKeys();
+			unsigned int nline = 0;
+			std::string line;
+			while (std::getline(inf, line))
+			{
+				++nline;
+				if (kVerbosity)
+					std::cout << "CardDealer: reading line " << nline << ": " << line << std::endl;
+
+				std::string key, token;
+				std::vector<std::string> words;
+
+				//removes all comments (anything after #)
+				if (line.find_first_of('#') != std::string::npos)
+					line.erase(line.find_first_of('#'));
+
+				if (line.empty())
+					continue;
+
+				std::stringstream ssl(line);
+				ssl >> key;	//key is first word
+
+				if (!ssl && kVerbosity)	//end of line
+				{
+					std::cout << "CardDealer: key " << key << " has empty argument" << std::endl;
+					continue;
+				}
+
+				while (!ssl.eof()) {
+					// string delimited by "..." or '...'
+					if (ssl.peek() == '\'' || ssl.peek() == '\"')
+					{
+						char end = ssl.peek();
+						ssl.ignore();
+
+						std::string word;
+						std::getline(ssl, word, end);
+						words.push_back(word);
+					}
+					// no delimiters, so expect words separated by commas
+					else if (!std::isspace(ssl.peek())) {
+						std::string word;
+						std::getline(ssl, word, ',');
+						//ssl >> word;
+						words.push_back(word);
+					}
+					else
+						ssl.ignore();
+				}
+				_entries[key] = words;
+
+				if (kVerbosity)
+					std::cout << "CardDealer: obtained key " << key << std::endl;
+			}
+		}
+
+		bool Find(const std::string &key) {
+			return _entries.find(key) != _entries.end();
+		}
+
+
+		// templated function that returns default if key is not found
+
+		
+		// empty template
+		//template <typename T>
+		//bool Get(const std::string &key, T &ret) {}
+
+		template <typename T>
+		struct is_sequence : std::false_type {};
+
+		template <typename ... Args>
+		struct is_sequence <std::vector<Args...> > : std::true_type {};
+
+		template <typename ... Args>
+		struct is_sequence <std::set<Args...> > : std::true_type {};
+
+		template <typename ... Args>
+		struct is_sequence <std::list<Args...> > : std::true_type {};
+
+		template <typename ... Args>
+		struct is_sequence <std::deque<Args...> > : std::true_type {};
+
+
+		// generic one, uses stringstream to cast string to correct type
+		// good for fundamental type
+		template <typename T, typename std::enable_if<std::is_fundamental<T>::value>::type* = nullptr>
+		bool Get(const std::string &key, T &ret) {
+			auto id = _entries.find(key);
+			if (id != _entries.end()) {
+				std::stringstream st(id->second.front());
+				return st >> ret;	// successful casting
+			}
+			else
+				return false;
+		}
+
+		// good for insert types
+		template <typename C, typename std::enable_if<is_sequence<C>::value>::type* = nullptr>
+		bool Get(const std::string &key, C &ret) {
+			auto id = _entries.find(key);
+			if (id != _entries.end()) {
+				ret.clear();
+				for (const std::string &it : id->second) {
+					typename C::value_type ty;
+					std::stringstream st(it);
+					if (st >> ty)
+						ret.insert(ret.end(), ty);
+				}
+				return bool(ret.size());
+			}
+			else
+				return false;
+		}
+
+
+		// return pieces of maps
+		template <typename T>
+		bool Get(const std::string &key, std::map<std::string, T> &ret) {
+			ret.clear();
+			for (const auto &id : _entries) {
+				if (id.first.find(key) != std::string::npos) {
+					T ty;
+ 					if (Get(id.first, ty))
+						ret[id.first.substr(id.first.find(key)
+								  + key.length())] = ty;
+				}
+			}
+
+			return bool(ret.size());
+		}
+
+		// for strings, there is no need to cast
+
+		bool Get(const std::string &key, std::string &ret) {
+			auto id = _entries.find(key);
+			if (id != _entries.end()) {
+				ret = id->second.front();
+				return true;
+			}
+			else
+				return false;
+		}
+
+		// for vectors of strings, there is no need to cast
+
+		bool Get(const std::string &key, std::vector<std::string> &ret) {
+			auto id = _entries.find(key);
+			if (id != _entries.end()) {
+				ret = id->second;
+				return bool(ret.size());
+			}
+			else
+				return false;
+		}
+
+
+		std::vector<std::string> ListKeys() {
+			std::vector<std::string> keys;
+			keys.reserve(_entries.size());
+			for (const auto &id : _entries)
+				keys.push_back(id.first);
+			return keys;
+		}
+
+
 
 	private:
-		std::map<std::string, std::vector<std::string> > key_strings;
-		std::map<std::string, std::vector<std::string> >::iterator ims;
+		std::map<std::string, std::vector<std::string> > _entries;
 
-		std::map<std::string, std::vector<double> > key_doubles;
-		std::map<std::string, std::vector<double> >::iterator imd;
-
-		bool kVerbosity, kStatus;
+		bool kVerbosity;
 		std::string _cardFile;
 };
 
