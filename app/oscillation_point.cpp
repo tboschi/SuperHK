274786346ab459767245da79cf3d000004d1e496
tboschi <tommaso.boschi@qmul.ac.uk>
2020-09-20 23:00:01 +0100

    minor fix

--- app/oscillation_point.cpp
+++ app/oscillation_point.cpp
@@ -1,22 +1,28 @@
-#include <fstream>
 #include <iostream>
 
 #include "physics/ParameterSpace.h"
 
 int main(int argc, char** argv)
 {
-	if (argc < 3)
+	if (argc < 1)
 	{
-		std::cerr << "Need two parameters: card output" << std::endl;
+		std::cerr << "Usage: card [parm1, parm2, ...]" << std::endl;
 		return 1;
 	}
 
-	std::string cardFile = argv[1], output = argv[2];
+	std::string cardFile = argv[1];
 	ParameterSpace *parms = new ParameterSpace(cardFile);
 
-	std::ofstream out(output);
-	out << parms->GetNominalEntry() << std::endl;
-	out.close();
+	if (argc < 2)
+		std::cout << parms->GetNominalEntry() << std::endl;
+	else {
+		std::vector<std::string> ps;
+		for (int f = 2; f < argc; ++f)
+			ps.push_back(std::string(argv[f]));
+		std::vector<int> entries = parms->GetScanEntries(ps);
+		for (int i : entries)
+			std::cout << i << std::endl;
+	}
 
 	delete parms;
 
