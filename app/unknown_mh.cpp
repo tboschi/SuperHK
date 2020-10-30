5048e55ce1aac614c2731a2956125e6b54080933
tboschi <tommaso.boschi@qmul.ac.uk>
2020-10-30 01:03:45 +0000

    fixed unknonw mh

--- app/unknown_mh.cpp
+++ app/unknown_mh.cpp
@@ -11,26 +11,28 @@ int main(int argc, char** argv)
 		return 1;
 	}
 
-	std::vector<double> x_dCP, x2_1, x2_2;
-	double dCP, sx2;
+	std::vector<double> x_dCP, tx2_1, cx2_1, tx2_2, cx2_2;
+	double dCP, sx2, tx2, cx2;
 
 	std::string line;
 	std::ifstream inf(argv[1]);
 	while (std::getline(inf, line)) {
 		std::stringstream ssl(line);
-		ssl >> dCP >> sx2;
+		ssl >> dCP >> sx2 >> tx2 >> cx2;
 
 		x_dCP.push_back(dCP);
-		x2_1.push_back(sx2);
+		tx2_1.push_back(tx2);
+		cx2_1.push_back(cx2);
 	}
 	inf.close();
 
 	inf.open(argv[2]);
 	while (std::getline(inf, line)) {
 		std::stringstream ssl(line);
-		ssl >> dCP >> sx2;
+		ssl >> dCP >> sx2 >> tx2 >> cx2;
 
-		x2_2.push_back(sx2);
+		tx2_2.push_back(tx2);
+		cx2_2.push_back(cx2);
 	}
 	inf.close();
 
@@ -38,7 +40,7 @@ int main(int argc, char** argv)
 	std::ofstream out(argv[3]);
 	for (int i = 0; i < x_dCP.size(); ++i)
 		out << x_dCP[i] << "\t"
-		    << std::min(x2_1[i], x2_2[i]) << "\n";
+		    << std::sqrt(std::min(tx2_1[i], tx2_2[i]) - std::min(cx2_1[i], cx2_2[i])) << "\n";
 	out.close();
 
 	return 0;
